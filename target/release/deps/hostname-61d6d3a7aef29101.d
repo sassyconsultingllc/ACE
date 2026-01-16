81b22c7fb5154b94ce964cca6b1675e443a682fd
sassyconsultingllc <sassyconsultingllc@gmail.com>
2026-01-16 00:11:01 -0600

    more updates

---
+++ target/release/deps/hostname-61d6d3a7aef29101.d
@@ -0,0 +1,9 @@
+V:\sassy-browser-FIXED\target\release\deps\hostname-61d6d3a7aef29101.d: C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\mod.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\bindings.rs
+
+V:\sassy-browser-FIXED\target\release\deps\libhostname-61d6d3a7aef29101.rlib: C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\mod.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\bindings.rs
+
+V:\sassy-browser-FIXED\target\release\deps\libhostname-61d6d3a7aef29101.rmeta: C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\lib.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\mod.rs C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\bindings.rs
+
+C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\lib.rs:
+C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\mod.rs:
+C:\Users\Admin\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\hostname-0.4.2\src\windows\bindings.rs:
